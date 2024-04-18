49a0b5fc9c8c45ebde8665c8a33ed469f29c188b
FujinoKeito <kd1335001@st.kobedenshi.ac.jp>
2024-04-18 10:00:04 +0900

    bugfix

--- javascript/post.js
+++ javascript/post.js
@@ -20,7 +20,7 @@ const convertedText = text.replace(/\r\n|\r|\n/g, '<br>');
 Result.innerHTML = convertedText;
 
 // フォームとボタンを非表示
-document.getElementById('contents').style.display = 'none';
+document.getElementById('post-container').style.display = 'none';
 });
 
 button1.addEventListener('click', () => {
