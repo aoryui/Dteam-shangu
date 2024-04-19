f601c450e7b4b2b1683ad445420f0e6a40d218c9
Haruki1203 <kd1309743@st.kobedenshi.ac.jp>
2024-04-19 09:30:05 +0900

    更新

--- javascript/question.js
+++ javascript/question.js
@@ -2,6 +2,9 @@
 const inputField = document.getElementById('inputField');
 const displayButton = document.getElementById('displayButton');
 const outputDiv = document.getElementById('output');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
 
 // ボタンのクリックイベントリスナーを設定
 displayButton.addEventListener('click', () => {
@@ -18,6 +21,21 @@ button1.addEventListener('click', () => {
 });
 
 button3.addEventListener('click', () => {
-    // ページ2に遷移
+    // 投稿に遷移
     window.location.href = 'post.html';
 });
+
+function getAnswer() {
+    // 質問入力欄から質問を取得
+    var questionInput = document.getElementById('questionInput');
+    var question = questionInput.value;
+    
+    // 回答表示用の要素を取得
+    var answerDisplay = document.getElementById('answerDisplay');
+    
+    // 質問に対する回答を設定（ここでは単純に質問に対するメッセージを設定）
+    var answer = "質問に対する回答: " + question;
+    
+    // 回答を表示
+    answerDisplay.innerHTML = answer;
+}
--- javascript/timeline.js
+++ javascript/timeline.js
@@ -1,11 +1,41 @@
-
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
 
 button3.addEventListener('click', () => {
-    // ページ2に遷移
+    // 投稿に遷移
     window.location.href = 'post.html';
 });
 
 button2.addEventListener('click', () => {
-    // ページ3に遷移
+    // 質問に遷移
     window.location.href = 'question.html';
 });
+
+// script.jsファイルの内容
+
+// タイムラインにイベントを追加する関数
+function addEvent() {
+    // イベントの時間と説明を入力から取得
+    const eventTime = document.getElementById('eventTime').value;
+    const eventDescription = document.getElementById('eventDescription').value;
+    
+    // タイムラインの要素を取得
+    const timeline = document.getElementById('timeline');
+    
+    // タイムラインイベントの要素を作成
+    const eventElement = document.createElement('div');
+    eventElement.classList.add('timeline-event');
+    
+    // 時間と説明を表示するための要素を追加
+    eventElement.innerHTML = `
+        <div><strong>${eventTime}</strong></div>
+        <div>${eventDescription}</div>
+    `;
+    
+    // タイムラインにイベントを追加
+    timeline.appendChild(eventElement);
+    
+    // 入力フォームをクリア
+    document.getElementById('eventTime').value = '';
+    document.getElementById('eventDescription').value = '';
+}
