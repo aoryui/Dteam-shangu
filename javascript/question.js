2d81370c51275172219e78460c1bf3cb70c1153b
HIGASINE Ryusei <kd1336693@st.kobedenshi.ac.jp>
2024-04-25 13:40:44 +0900

    次こそ

--- javascript/question.js
+++ javascript/question.js
@@ -2,28 +2,29 @@
 const inputField = document.getElementById('inputField');
 const displayButton = document.getElementById('displayButton');
 const outputDiv = document.getElementById('output');
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
+const button1 = document.getElementById('timeline');
+const button2 = document.getElementById('question');
+// const button3 = document.getElementById('button3');
 
 // ボタンのクリックイベントリスナーを設定
-displayButton.addEventListener('click', () => {
-    // 入力フィールドから値を取得
-    const inputValue = inputField.value;
+// displayButton.addEventListener('click', () => {
+//     // 入力フィールドから値を取得
+//     const inputValue = inputField.value;
     
-    // 取得した値を表示エリアに表示
-    outputDiv.textContent = inputValue;
-});
+//     // 取得した値を表示エリアに表示
+//     outputDiv.textContent = inputValue;
+// });
 
 button1.addEventListener('click', () => {
-    // ページ1に遷移
+    // 投稿に遷移
     window.location.href = 'timeline.html';
 });
 
-button3.addEventListener('click', () => {
-    // 投稿に遷移
-    window.location.href = 'post.html';
+button2.addEventListener('click', () => {
+    // 質問に遷移
+    window.location.href = 'question.html';
 });
+
 
 function getAnswer() {
     // 質問入力欄から質問を取得
