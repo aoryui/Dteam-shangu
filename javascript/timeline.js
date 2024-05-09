50e57db126431998baf835c5db0014774cb621e8
FujinoKeito <kd1335001@st.kobedenshi.ac.jp>
2024-05-09 14:13:47 +0900

    いいね数表示

--- javascript/timeline.js
+++ javascript/timeline.js
@@ -17,8 +17,6 @@ button3.addEventListener('click', () => {
   window.location.href = 'post.html';
 });
 
-
-
 var PostText = {
   1:"AWSの始め方",
   2:"Dockerが開発を変える！",
@@ -42,6 +40,15 @@ var PostTime4 = {1:"2024", 2:"03", 3:"02"}
 var PostTime5 = {1:"2024", 2:"02", 3:"29"}
 var PostTime6 = {1:"2024", 2:"02", 3:"28"}
 var PostTime7 = {1:"2024", 2:"02", 3:"20"}
+var PostGood = {
+  1:5241,
+  2:21,
+  3:2342,
+  4:542,
+  5:2312,
+  6:3,
+  7:2423,
+}
 var PostArticle = {
   1:"hoge",
   2:"huga",
@@ -88,6 +95,14 @@ for (let i = 1; i <= numberOfKeys; i++) {
     // li要素をdiv要素の子要素として追加
     divElement.appendChild(liElement);
   }
+
+  // Good表示
+  const GoodElement = document.createElement('p'); // <p id="good-number">を作成
+  GoodElement.id = 'good-number';
+  GoodElement.textContent = PostGood[i];
+  divElement.appendChild(GoodElement);
+  const GoodIcon = '<svg id="good-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M2 8.99997H5V21H2C1.44772 21 1 20.5523 1 20V9.99997C1 9.44769 1.44772 8.99997 2 8.99997ZM7.29289 7.70708L13.6934 1.30661C13.8693 1.13066 14.1479 1.11087 14.3469 1.26016L15.1995 1.8996C15.6842 2.26312 15.9026 2.88253 15.7531 3.46966L14.5998 7.99997H21C22.1046 7.99997 23 8.8954 23 9.99997V12.1043C23 12.3656 22.9488 12.6243 22.8494 12.8658L19.755 20.3807C19.6007 20.7554 19.2355 21 18.8303 21H8C7.44772 21 7 20.5523 7 20V8.41419C7 8.14897 7.10536 7.89462 7.29289 7.70708Z"></path></svg>';
+  divElement.innerHTML += GoodIcon;
 }
 
 // find関数を使った検索フォーム
